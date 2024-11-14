f92dbd76fe3d9b5d5b9de67b883536fba7396678
esong21 <96155582+esong21@users.noreply.github.com>
2024-11-14 15:36:51 -0500

    Added a pinch of dis anddat to tft.c (basicaly just joqpgj i43qpj94;j3aqjkl;gr w)

--- src/tft.c
+++ src/tft.c
@@ -2,5 +2,9 @@
 #include <stdint.h>
 
 void init_spi1_slow(){
-    // i donut know wut i' em doin; howd'y parner
+    RCC -> AHBENR |= RCC_AHBENR_GPIOBEN;
+    GPIOB -> MODER &= ~(GPIO_MODER_MODER3 || GPIO_MODER_MODER4 || GPIO_MODER_MODER5);
+    GPIOB -> MODER |= (GPIO_MODER_MODER3_0 || GPIO_MODER_MODER4_0 || GPIO_MODER_MODER5_0);
+    GPIOB -> BSRR |= GPIO_BSRR_BR_3;
+    
 }
