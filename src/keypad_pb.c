21a7f9802ccc9e8a219d55349305f131a6ce3310
esong21 <96155582+esong21@users.noreply.github.com>
2024-11-15 09:38:18 -0500

    Finished code for two pushbuttons for enabling entering password and entering the passcode

--- src/keypad_pb.c
+++ src/keypad_pb.c
@@ -3,6 +3,8 @@
 
 void enable_keypad_ports();
 void keypad();
+void enterPassword();
+void EXTI4_15_IRQHandler();
 
 void enable_keypad_ports() {
     //enabling RCC
@@ -48,8 +50,35 @@ void keypad(void){
     //   setn(9, 0);
     //   setn(10, 0);
     //   setn(11, 0);
-    // }
-    
+    // }  
   }
 }
 
+volatile uint32_t button_pressed = 0; // Variable to store the value when a button is pressed
+  
+void enterPassword() {
+  //Using PC9-10 as inputs for push buttons
+  RCC -> AHBENR |= RCC_AHBENR_GPIOCEN;
+  RCC -> APB2ENR |= RCC_APB2ENR_SYSCFGCOMPEN;
+  GPIOC -> MODER &= ~(GPIO_MODER_MODER9_1 | GPIO_MODER_MODER10_1);
+  GPIOC -> PUPDR |= (GPIO_PUPDR_PUPDR9_1 | GPIO_PUPDR_PUPDR10_1);
+
+  SYSCFG -> EXTICR[2] |= 0x0020;
+  SYSCFG -> EXTICR[2] |= 0x0200;
+
+  EXTI -> RTSR |= (EXTI_RTSR_RT9 | EXTI_RTSR_RT10);
+  EXTI -> IMR |= (EXTI_IMR_IM9 | EXTI_IMR_IM10);
+  NVIC -> ISER[0] |= 1<<EXTI4_15_IRQn;
+}
+
+void EXTI4_15_IRQHandler(void) {
+  if(EXTI -> PR & EXTI_PR_PR9) {
+    EXTI -> PR |= EXTI_PR_PR9;
+    button_pressed = 1;
+  }
+
+  if(EXTI -> PR & EXTI_PR_PR10) {
+    EXTI -> PR |= EXTI_PR_PR10;
+    button_pressed = 1;
+  }
+}
